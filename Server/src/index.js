28e0fe579475ea79d3dab8b414d52d7de445d71d
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2023-10-25 18:42:52 -0300

    25/10/23/18:42

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,11 +1,33 @@
 const http = require('http');
+const PORT = 3001
+const character = require("./utils/data");
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
-  });
+
+http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const {url} = req;
+
+if (url.includes("/rickandmorty/character")) {
+  let urlArray = url.split("/")
+  let id = Number(urlArray.pop())
+
+  let foundCh = character.find((ch)=> ch.id === id)
   
-  server.listen(3001, () => {
-    console.log('Servidor en ejecución en el puerto 3001');
-  });
+
+  if (foundCh) {
+  res.writeHead(200, {"Content-Type": "application/json"})
+  res.end(JSON.stringify(foundCh))
+  }else{
+  res.writeHead(404, {"Content-Type": "application/json"})
+  res.end(JSON.stringify({error: "error"}))
+  }
+}
+
+
+
+})
+.listen(PORT, "localhost");
   
+ 
